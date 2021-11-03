1341910d8bb087b17e3d86f4ba50e83a6bb5069c
Steven-Cadena <steven.cadena.giler@gmail.com>
2021-11-03 10:41:57 +0100

    comiit

--- src/components/Navegacion/Multiplicar.vue
+++ src/components/Navegacion/Multiplicar.vue
@@ -1,9 +1,23 @@
 <template>
     <div>
         <h1>Multiplicar</h1>
+        <!-- SOLUCION CON TABLA -->
         <label>Introduzca un número:</label>
         <input type="number" v-model="numero" />
-         <ul v-if="resultado.length != 0">
+        <button @click="mostrarTabla()">
+            tabla multiplicar
+        </button>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Operación</th>
+                    <th>Resultado</th>
+                </tr>
+            </thead>
+            <tbody v-html="tabla">{{tabla}}</tbody>
+        </table>
+        <!--SOLUCION SIN TABLA
+          <ul v-if="resultado.length != 0">
             <li v-for="resul in resultado" :key="resul">
                 {{resul}}
             </li>
@@ -11,27 +25,48 @@
         <button @click="multiplicar(numero)">
             Multiplicar
         </button>
+        <hr/> -->
     </div>
 </template>
 
 <script>
 export default {
     name:"Multiplicar",
+    // SOLUCION CON TABLA
     methods: {
-        multiplicar(num){
-             for (var i = 1; i<=10; i++){
-                 var result = num * i;
-                 this.resultado.push(result);
-                 console.log("entra aquí");
-             }
-            return this.resultado;
+        mostrarTabla (){
+            this.tabla='';
+            for (var i = 1; i<=10; i++){
+                var op = parseInt(this.numero) * i;
+                this.tabla += "<tr>";
+                this.tabla += "<td>" + this.numero + "*" + i + "</td>";
+                this.tabla += "<td>" + op + "</td>";
+                this.tabla += "</tr>";
+            }
         }
     },
     data(){
         return{
-            resultado:[],
-            numero:0
+            numero:0,
+            tabla:""
         }
     }
+    //SOLUCION SIN TABLA
+    // methods: {
+    //     multiplicar(num){
+    //          for (var i = 1; i<=10; i++){
+    //              var result = num * i;
+    //              this.resultado.push(result);
+    //              console.log("entra aquí");
+    //          }
+    //         return this.resultado;
+    //     }
+    // },
+    // data(){
+    //     return{
+    //         resultado:[],
+    //         numero:0,
+    //     }
+    // }
 }
 </script>
